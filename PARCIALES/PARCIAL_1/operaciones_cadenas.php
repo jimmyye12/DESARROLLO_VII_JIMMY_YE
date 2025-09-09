cc86d3335fa70a3b72a9c9734a63119a092cedf2
Jimmy Ye <jimmy.ye@utp.ac.pa>
2025-09-08 20:05:35 -0500

    parcial 1

---
+++ PARCIALES/PARCIAL_1/operaciones_cadenas.php
@@ -0,0 +1,17 @@
+<?php
+// contar palabras repetidas
+
+$texto = "NUEVE por nueve igual a ochenta y uno";
+$textoenminusculas = strtolower($texto);
+$partes = explode(" ", $textoenminusculas);
+print_r($partes);
+
+for ($i = 0; $i < count ($partes); $i++){
+if ($partes [$i] == $partes) {
+echo 1;
+
+}
+}
+
+?>
+      
