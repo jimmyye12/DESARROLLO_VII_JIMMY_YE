f45d2283a8d4660cf368804ae9551362a8e6c887
Jimmy Ye <jimmy.ye@utp.ac.pa>
2025-09-21 11:58:15 -0500

    Paso 1: Implementación de la clase Libro

---
+++ PARCIALES/PARCIAL_1/gestionar_membresia.php
@@ -0,0 +1,17 @@
+<?php
+include 'funciones_gimnasio.php';
+
+$membresias = ['basica' => 80 , 'premium' => 120, 'vip' => 180, 'familiar' => 250, 'corporativa' => 300];
+
+$miembro = [
+ 'Ernesto Perez' => ['tipo' => 'premium', 'antiguedad' => 15],
+ 'Alexander Franco' => ['tipo' => 'basica', 'antiguedad' => 10],
+ 'Yahir Lopez' => ['tipo' => 'vip', 'antiguedad' => 6],
+ 'Frank Degracia' => ['tipo' => 'familiar', 'antiguedad' => 2],
+ 'Dan Gonzalez' => ['tipo' => 'premium', 'antiguedad' => 30],
+ ]
+
+ foreach ($miembro as $miembros){
+    
+ }
+?>
