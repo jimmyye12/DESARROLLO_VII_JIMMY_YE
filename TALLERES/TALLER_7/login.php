4656eb2c673eb5d3fc4f8966be4e49d9cf08b9d4
Jimmy Ye <jimmy.ye@utp.ac.pa>
2025-10-13 18:47:25 -0500

    TALLER_7 - Paso 4: Implementación de medidas de seguridad para cookies y sesiones

--- TALLERES/TALLER_7/login.php
+++ TALLERES/TALLER_7/login.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+include 'config_sesion.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+        die("Error de validación CSRF");
+    }
+}
+$_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 
 // Si ya hay una sesión activa, redirigir al panel
 if(isset($_SESSION['usuario'])) {
@@ -41,6 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <input type="text" id="usuario" name="usuario" required><br><br>
         <label for="contrasena">Contraseña:</label><br>
         <input type="password" id="contrasena" name="contrasena" required><br><br>
+         <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
         <input type="submit" value="Iniciar Sesión">
     </form>
 </body>
